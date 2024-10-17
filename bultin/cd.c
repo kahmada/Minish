f3e66300cfad78818a4a1b4eb2058a4b7455b91f
Khadijaahmada <khadijaahmada148@gmail.com>
2024-10-17 14:54:26 +0100

    hi

---
+++ bultin/cd.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kahmada <kahmada@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 14:27:16 by kahmada           #+#    #+#             */
+/*   Updated: 2024/10/16 16:30:17 by kahmada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	update_envp(t_env **envp, char *key, char *value)
+{
+	t_env	*tmp;
+
+	tmp = *envp;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = ft_strdup(value);
+			return ;
+		}
+		tmp = tmp->next;
+	}
+}
+
+char	*get_home(t_env **envp)
+{
+	t_env	*current;
+
+	current = *envp;
+	while (current)
+	{
+		if (ft_strcmp(current->key, "HOME") == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
+
+char	*get_home_path(t_command *cmd, t_env **envp)
+{
+	char	*path;
+
+	path = get_home(envp);
+	if (!path)
+	{
+		ft_putstr_fd("Minishell: cd: HOME not set\n", 2);
+		cmd->ex = manage_exit_status(1, 1);
+		free(cmd->ex);
+		return (NULL);
+	}
+	return (path);
+}
+
+void	bult_cd(t_command *cmd, t_env **envp)
+{
+	char	*path;
+	char	*oldpath;
+	char	*new_path;
+
+	oldpath = getenv("PWD");
+	if (cmd->args[1] == NULL)
+	{
+		path = get_home_path(cmd, envp);
+		if (!path)
+			return ;
+	}
+	else
+		path = cmd->args[1];
+	cmd->ret = chdir(path);
+	if (cmd->ret == -1)
+	{
+		perror("minishell");
+		free(manage_exit_status(1, 1));
+		return ;
+	}
+	free(manage_exit_status(0, 1));
+	update_envp(envp, "OLDPWD", oldpath);
+	new_path = getcwd(NULL, 0);
+	update_envp(envp, "PWD", new_path);
+	free(new_path);
+}
---
+++ execution/ex_func.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex_func.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kahmada <kahmada@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 17:47:43 by kahmada           #+#    #+#             */
+/*   Updated: 2024/10/16 15:49:56 by kahmada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	handle_parent_signals(t_command *cmd)
+{
+	if (cmd->fd_in != 0)
+		close(cmd->fd_in);
+	signal(SIGINT, SIG_IGN);
+	cmd->ex = manage_exit_status(127, 1);
+	free(cmd->ex);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+char	**handle_builtin_cmd(t_command *cmd, char **envp)
+{
+	handle_redirects(cmd);
+	envp = handle_builtin(cmd, envp);
+	return (envp);
+}
+
+char	**handle_builtin_cmd_out(t_command *cmd, char **envp)
+{
+	handle_redirects(cmd);
+	envp = handle_built_out(cmd, envp);
+	return (envp);
+}
+
+void	handle_inside_minishel(t_command *cmd)
+{
+	if (cmd->args[0][0] == '.')
+	{
+		close(0);
+		close(1);
+	}
+}
